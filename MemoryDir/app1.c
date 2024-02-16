a66848c8383a16d645e8fc8f58984c545f968b10
Cianolin <145426146+Cianolin@users.noreply.github.com>
2024-02-16 13:16:27 +0100

    aggiuta della malloc
    aggiuta della malloc usando un puntatore

--- MemoryDir/app1.c
+++ MemoryDir/app1.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
+#include <malloc.h>
 int global1= 130;
 int global2;
-int main(){
-    int a;
+int main()
+{
+    int a,numero;
+   // int *b;
+    int *b = malloc(4096);
+    //numero=10;
+    //b=(int*)malloc(sizeof(int)*numero);
     while(1){
         printf("%d\n", getpid());
         printf("%p\n",&a);
+        printf("%p\n",&b);
         sleep(1);
     }
+    //free(b);
     return 0;
 }
+/*
+7ffe20ea4000-7ffe20ec6000
+l'inizio 7ffe20ea4000
+7ffe20ec6000 fine 
+sono dello  stack
+
+7ffd019fd000 
+7ffd01a1cc24
+7ffd01a1f000
+*/
