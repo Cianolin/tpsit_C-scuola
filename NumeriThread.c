683d2b6e89ab94a6b3578b988916bba4baf97308
Luciano Lin <luciano.lin@iisviolamarchesini.edu.it>
2024-04-17 16:59:20 +0200

    trova il numero con i thread

--- NumeriThread.c
+++ NumeriThread.c
@@ -2,15 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
-void* PrimaMeta(void* par){
-int* nArray= &par;
+void *PrimaMeta(void *par)
+{
+  int *firstHalf = (int *)par;
+  for (int i = 0; i < 5; i++)
+  {
+    if(firstHalf[i]==8){
+      printf("posizione %d\n", i);
+    }
+  }
 }
-void* SecondaMeta(void* par){
-
+void *SecondaMeta(void *par)
+{
+  int *secondHalf = (int *)par;
+  for (int i = 0; i < 5; i++)
+  {
+    if(secondHalf[i]==8){
+      printf("posizione %d\n", i+5);
+    }
+  }
 }
-int main(){
+int main()
+{
   pthread_t threadA, threadB;
-  int numeri[]={1,2,3,4,5,6,7,8,9,10};
-  pthread_create()
+  int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int *firstHalf = array;
+  int *secondHalf = array + 5;
+  pthread_create(&threadA, NULL, &PrimaMeta, (void *)firstHalf);
+  pthread_create(&threadB, NULL, &SecondaMeta, (void *)secondHalf);
+  pthread_join(threadB, NULL);
+  pthread_join(threadA, NULL);
   return 0;
 }
