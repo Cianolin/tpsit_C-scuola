1b2d9aecde8de63a4de7c2058b01fca8fe30e226
Cianolin <145426146+Cianolin@users.noreply.github.com>
2024-04-17 13:43:25 +0200

    Update NumeriThread.c

--- NumeriThread.c
+++ NumeriThread.c
@@ -2,9 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
+void* PrimaMeta(void* par){
+int* nArray= &par;
+}
+void* SecondaMeta(void* par){
 
+}
 int main(){
   pthread_t threadA, threadB;
   int numeri[]={1,2,3,4,5,6,7,8,9,10};
+  pthread_create()
   return 0;
 }
