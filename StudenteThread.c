c612681588746a93116a9c18ff388d36d05c7917
Cianolin <145426146+Cianolin@users.noreply.github.com>
2024-04-17 13:17:09 +0200

    Update StudenteThread.c

--- StudenteThread.c
+++ StudenteThread.c
@@ -10,38 +10,34 @@ typedef struct
     int class;
     int media;
 } Studente;
-// void *stampaStudente(void *par1,void* par2,void *par3, void *par4)
 void *stampaStudente(void* par)
 {
-    Studente *s2 = (Studente *)par;
+    Studente *s2 = (Studente *)par;//converte il void in studente puntatore
     printf("\n%s\n", s2->name);
     printf("%s\n", s2->lastname);
     printf("%d\n", s2->class);
-    printf("%d\n", s2->class);
+    printf("%d\n", s2->media);
 }
 void *salvaStudente (void* par)
 {
-    Studente *s3 = (Studente *)par;stampaStudente
-    fwrite(&s3, sizeof(s3), 1, file);
-    fclose(file);
+    FILE*file=fopen("studente.txt", "wb");
+    Studente* s3 = (Studente* )par;stampaStudente
+    fwrite(&s3, sizeof(s3), 1, file);//scrive nel file la struct studente
+    fclose(file);//chiude il file
     printf("Scrittura fatta\n");
 }stampaStudente
 int main(int argc, char *argv[])
 {
-    FILE* fileSorgente;
     Studente s1;
-    pthread_t threadstampaStudente, threadB;
-    strcpy(s1.name, argv[1]);fileSorgente= fopen()
+    pthread_t threadA, threadB;
+    strcpy(s1.name, argv[1]);
     strcpy(s1.lastname, argv[2]);
     s1.media = atoi(argv[3]);
     s1.class = atoi(argv[4]);
-    void *sPuntore = &s1;
-    pthread_create(&threadA, NULL, &stampaStudente, sPuntore);
-    pthread_create(&threadB, NULL, &salvaStudente, sPuntore);
+    void* sPuntore = &s1;//converto la strct studente in void puntatore
+    pthread_create(&threadA, NULL, &stampaStudente, sPuntore);//crea threadA viene assegnato la stampa dei dati dello stundete
+    pthread_create(&threadB, NULL, &salvaStudente, sPuntore);//crea threadB salva la struct dello studente salva i dati
     pthread_join(threadA, NULL);
     pthread_join(threadB, NULL);
-    
-    
-
     return 0;
 }
